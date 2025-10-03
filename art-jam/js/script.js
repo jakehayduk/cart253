2af8faad801df834b758693f0283c4024d8912d5
jakehayduk <j_hayduk@live.concordia.ca>
2025-10-02 21:17:42 -0400

    Drew the character on the screen

--- art-jam/js/script.js
+++ art-jam/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Art Jam Portrait
+ * Jake Hayduk
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -8,11 +8,16 @@
 
 "use strict";
 
+const jake = {
+  x: undefined, // will be mouseX
+  y: undefined // will be mouseY
+};
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create the canvas
 */
 function setup() {
-
+    createCanvas(500, 500);
 }
 
 
@@ -20,5 +25,133 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(230, 230, 230);
 
+    drawBody();
+}
+
+function drawBody() {
+
+    // Neck
+    push();
+    noStroke();
+    fill(245, 215, 205);
+    rect(244, 145, 12, 30, 10);
+    pop();
+
+    // Head
+    push();
+    noStroke();
+    fill(250, 220, 210);
+    rect(230, 100, 40, 60, 30);
+    pop();
+
+    // Hair
+    push();
+    noStroke();
+    fill(170, 120, 120);
+    rect(230, 100, 40, 20, 20);
+    pop();
+
+    // Right eye
+    push();
+    noStroke();
+    fill(255, 255, 255);
+    ellipse(242, 130, 10);
+    pop();
+
+    // Left eye
+    push();
+    noStroke();
+    fill(255, 255, 255);
+    ellipse(258, 130, 10);
+    pop();
+
+    // Right pupil
+    push();
+    noStroke();
+    fill(25, 5, 0);
+    ellipse(242, 130, 6);
+    pop();
+
+    // Left pupil
+    push();
+    noStroke();
+    fill(25, 5, 0);
+    ellipse(258, 130, 6);
+    pop();
+
+    // Mouth
+    push();
+    noStroke();
+    fill(40, 10, 10);
+    rect(244, 145, 12, 3, 10);
+    pop();
+
+    // Right Leg
+    push();
+    noStroke();
+    fill(120, 120, 120);
+    rect(225, 250, 20, 120, 20);
+    pop();
+
+    // Left Leg
+    push();
+    noStroke();
+    fill(120, 120, 120);
+    rect(255, 250, 20, 120, 20);
+    pop();
+
+    // Torso
+    push();
+    noStroke();
+    fill(30, 30, 30);
+    rect(220, 170, 60, 100, 20);
+    pop();
+
+    // Right Foot
+    push();
+    noStroke();
+    fill(30, 30, 30);
+    rect(215, 350, 30, 20, 20);
+    pop();
+
+    // Left Foot
+    push();
+    noStroke();
+    fill(30, 30, 30);
+    rect(255, 350, 30, 20, 20);
+    pop();
+
+    // Right Arm
+    push();
+    noStroke();
+    fill(30, 30, 30);
+    angleMode(DEGREES);
+    rotate(20);
+    rect(260, 90, 20, 80, 20);
+    pop();
+
+    // Left Arm
+    push();
+    noStroke();
+    fill(30, 30, 30);
+    angleMode(DEGREES);
+    rotate(-20);
+    rect(190, 260, 20, 80, 20);
+    pop();
+
+    // Right eye
+    push();
+    noStroke();
+    fill(250, 220, 210);
+    ellipse(195, 250, 20);
+    pop();
+
+    // Left eye
+    push();
+    noStroke();
+    fill(250, 220, 210);
+    ellipse(305, 250, 20);
+    pop();
 }
