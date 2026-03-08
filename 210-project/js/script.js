93dd9ae2b17a6dc532f18ec33f44203597f857fc
jakehayduk <j_hayduk@live.concordia.ca>
2026-03-08 19:10:22 -0400

    cart 210 project

--- 210-project/js/script.js
+++ 210-project/js/script.js
@@ -1,5 +1,5 @@
 /**
- * Variation Jam
+ * CART-210 Exploration Game
  * Jake Hayduk
  * 
  * Online multiplayer game that has basic features
@@ -11,28 +11,13 @@
 /**
  * Sets up the canvas and loads the background image
 */
-
+let bg;
 function setup() {
     createCanvas(800, 600);
     noSmooth();
-    music.loop();
+    // music.loop();
+    bg = loadImage('./assets/images/background-2.gif');
 }
-
-// Handles the background music
-// function backgroundMusic() {
-//     song[songNumber].setVolume(backgroundMusicVol);
-//     song[songNumber].play();
-//     song[songNumber].onended(nextSong);
-// }
-
-// Goes to the next background song
-// function nextSong() {
-//     songNumber++;
-//     if (songNumber > 2) {
-//         songNumber = 0;
-//     }
-//     backgroundMusic();
-// }
 
 // Variable definitions
 let playerSpeed = 5;
@@ -56,9 +41,20 @@ function randomGen(min, max) {
     return Math.floor(Math.random() * (Math.ceil(max) - Math.ceil(min) + 1)) + Math.ceil(min);
 }
 
+let camera = {
+    x: 400,
+    y: 300,
+    x2: 400,
+    y2: 300,
+    x3: 400,
+    y3: 300,
+}
+
 let player = {
-    x: randomGen(200, 600),
-    y: randomGen(150, 450),
+    x: 400,
+    y: 300,
+    x2: 400,
+    y2: 300,
     size: 64,
     direction: "F",
     moving: false,
@@ -68,85 +64,14 @@ let player = {
 let heart = {
     x: 150,
     y: 100,
+    x2: 150,
+    y2: 100,
     size: 64
 }
 
-/**
- * Draws the images and objects on the screen (more in detail below)
-*/
-function draw() {
-    background(180, 150, 130);
-        // Left
-        if (keyIsDown(65) || keyIsDown(LEFT_ARROW)) {
-            if (player.x > 0) {
-                player.x -= playerSpeed;
-                player.direction = "L";
-                player.moving = true;
-
-                if (!soundIsPlaying) {
-                    sound3.rate(2);
-                    sound3.loop();
-                    soundIsPlaying = true;
-                }
-            }
-            innactiveTimer = 0;
-        }
-
-        // Right
-        else if (keyIsDown(68) || keyIsDown(RIGHT_ARROW)) {
-            if (player.x < width) {
-                player.x += playerSpeed;
-                player.direction = "R";
-                player.moving = true;
-                
-                if (!soundIsPlaying) {
-                    sound3.rate(2);
-                    sound3.loop();
-                    soundIsPlaying = true;
-                }
-            }
-            innactiveTimer = 0;
-        }
-
-        // Up
-        else if (keyIsDown(87) || keyIsDown(UP_ARROW)) {
-            if (player.y > 0) {
-                player.y -= playerSpeed;
-                player.direction = "B";
-                player.moving = true;
-
-                if (!soundIsPlaying) {
-                    sound3.rate(2);
-                    sound3.loop();
-                    soundIsPlaying = true;
-                }
-            }
-            innactiveTimer = 0;
-        }
-
-        // Down
-        else if (keyIsDown(83) || keyIsDown(DOWN_ARROW)) {
-            if (player.y < height) {
-                player.y += playerSpeed;
-                player.direction = "F";
-                player.moving = true;
-
-                if (!soundIsPlaying) {
-                    sound3.rate(2);
-                    sound3.loop();
-                    soundIsPlaying = true;
-                }
-            }
-            innactiveTimer = 0;
-        }
-
-        else {
-            player.moving = false;
-            sound3.stop();
-            soundIsPlaying = false;
-        }
-
-        drawPlayer();
+let arrow = {
+    x: 150,
+    y: 100
 }
 
 // Defining asset variables
@@ -161,6 +86,7 @@ let img8;
 let img9;
 let img10;
 let img11;
+let img12;
 
 let font1;
 
@@ -183,6 +109,7 @@ function preload() {
     img9 = loadImage('./assets/images/sleep.gif');
     img10 = loadImage('./assets/images/heart.gif');
     img11 = loadImage('./assets/images/letter.gif');
+    img12 = loadImage('./assets/images/arrow.gif');
 
     // Load fonts
     font1 = loadFont('./assets/fonts/Micro-5.ttf');
@@ -194,16 +121,161 @@ function preload() {
     music = loadSound('./assets/sounds/sound-4.mp3');
 }
 
+/**
+ * Draws the images and objects on the screen (more in detail below)
+*/
+function draw() {
+    background(255);
+    image(bg, camera.x3 - width/2 * 5, camera.y3 - height/2 * 5, width * 5, height * 5);
+    
+        // Left
+        if (keyIsDown(65) || keyIsDown(LEFT_ARROW)) {
+            if (camera.x > -(width/2 * 4)) {
+                player.x -= playerSpeed;
+                camera.x -= playerSpeed;
+                player.direction = "L";
+                player.moving = true;
+
+                if (!soundIsPlaying) {
+                    sound3.rate(2);
+                    sound3.loop();
+                    soundIsPlaying = true;
+                }
+            }
+            innactiveTimer = 0;
+        }
+
+        // Right
+        else if (keyIsDown(68) || keyIsDown(RIGHT_ARROW)) {
+            if (camera.x < (width/2 * 6)) {
+                player.x += playerSpeed;
+                camera.x += playerSpeed;
+                player.direction = "R";
+                player.moving = true;
+                
+                if (!soundIsPlaying) {
+                    sound3.rate(2);
+                    sound3.loop();
+                    soundIsPlaying = true;
+                }
+            }
+            innactiveTimer = 0;
+        }
+
+        // Up
+        else if (keyIsDown(87) || keyIsDown(UP_ARROW)) {
+            if (camera.y > -(height/2 * 4)) {
+                camera.y -= playerSpeed;
+                player.y -= playerSpeed;
+                player.direction = "B";
+                player.moving = true;
+
+                if (!soundIsPlaying) {
+                    sound3.rate(2);
+                    sound3.loop();
+                    soundIsPlaying = true;
+                }
+            }
+            innactiveTimer = 0;
+        }
+
+        // Down
+        else if (keyIsDown(83) || keyIsDown(DOWN_ARROW)) {
+            if (camera.y < (height/2 * 6)) {
+                player.y += playerSpeed;
+                camera.y += playerSpeed;
+                player.direction = "F";
+                player.moving = true;
+
+                if (!soundIsPlaying) {
+                    sound3.rate(2);
+                    sound3.loop();
+                    soundIsPlaying = true;
+                }
+            }
+            innactiveTimer = 0;
+        }
+
+        else {
+            player.moving = false;
+            sound3.stop();
+            soundIsPlaying = false;
+        }
+
+        player.x = lerp(player.x, player.x2, 0.05);
+        player.y = lerp(player.y, player.y2, 0.05);
+
+        player.x2 = width/2;
+        player.y2 = height/2;
+
+        
+
+        // console.log(player.x);
+        // camera.x = player.x;
+        // camera.y = player.y;
+        camera.x2 = lerp(camera.x2, camera.x, 0.05);
+        camera.y2= lerp(camera.y2, camera.y, 0.05);
+
+        // console.log(camera.x2 + ", " + camera.y2);
+
+        // camera.x2 = width/2;
+        // camera.y2 = height/2;
+        camera.x3 = camera.x2 * -1 + width;
+        camera.y3 = camera.y2 * -1 + height;
+
+        heart.x = heart.x2 + camera.x3;
+        heart.y = heart.y2 + camera.y3;
+        
+        push();
+        fill(220, 0, 40);
+        noStroke();
+        arrow.x = heart.x;
+        arrow.y = heart.y;
+        if (heart.x > width - 30) {
+            arrow.x = width - 30;
+        }
+
+        if (heart.x < 0 + 30) {
+            arrow.x = 0 + 30;
+        }
+
+        if (heart.y > height - 30) {
+            arrow.y = height - 30;
+        }
+
+        if (heart.y < 0 + 30) {
+            arrow.y = 0 + 30;
+        }
+        
+        angleMode(DEGREES);
+        translate(arrow.x, arrow.y);
+        rotate((atan2(heart.y - arrow.y, heart.x - arrow.x)))
+        translate(-arrow.x, -arrow.y);
+        if (heart.x > width + 30 || heart.x < 0 - 30 || heart.y > height + 30 || heart.y < 0 - 30) {
+            // triangle(arrow.x - 15, arrow.y - 15, arrow.x + 15, arrow.y, arrow.x - 15, arrow.y + 15);
+            image(img12, arrow.x - 16, arrow.y - 16, 32, 32);
+        }
+        
+        pop();
+        console.log(camera.x + ", " + camera.y);
+
+        if (camera.x2 < -(width/2 * 4)) {
+            camera.x2 = -(width/2 * 4);
+        }
+        
+        drawPlayer();
+}
+
 // Draws each player on the screen using the gif images and some text
 function drawPlayer() {
 
-    push();
-    textSize(35);
-    fill(0, 0, 0, 50);
-    textFont(font1);
-    textAlign(CENTER);
-    text("Collect hearts to heal the Jake!", 400, 300)
-    pop();
+    // push();
+    // textSize(35);
+    // fill(0, 0, 0, 50);
+    // textFont(font1);
+    // textAlign(CENTER);
+    // text("Collect hearts to heal the Jake!", 400, 300)
+    // pop();
 
     // Each of these if statements handles a different direction that is stored in each player object, changing the image depending on the direction and if it is moving or not
     if (player.direction == "F") {
@@ -282,24 +354,26 @@ function drawPlayer() {
         if (hearts < 6) {
             hearts++;
             sound2.play();
-            heart.x = width - player.x + randomGen(0, 200) - randomGen(0, 200);
-            heart.y = height - player.y + randomGen(0, 200) - randomGen(0, 200);
+            heart.x2 = randomGen(-1600, 1600 + width/2);
+            heart.y2 = randomGen(-1200, 1200 + height/2);
 
-            if (heart.x > 780) {
-                heart.x = 760;
-            }
+            console.log(heart.x2 + ", " + heart.y2);
 
-            if (heart.y > 580) {
-                heart.y = 560;
-            }
+            // if (heart.x > 780) {
+            //     heart.x = 760;
+            // }
 
-            if (heart.x < 20) {
-                heart.x = 40;
-            }
+            // if (heart.y > 580) {
+            //     heart.y = 560;
+            // }
 
-            if (heart.y < 20) {
-                heart.y = 40;
-            }
+            // if (heart.x < 20) {
+            //     heart.x = 40;
+            // }
+
+            // if (heart.y < 20) {
+            //     heart.y = 40;
+            // }
         }
     }
 
